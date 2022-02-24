c3decb2ca03b8577a7f09360d7be4bdacbea8755
egersack <egersack@mit.edu>
2022-02-24 11:50:14 -0500

    commit

--- PS2.py
+++ PS2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 
 # raw data from train.csv
 url = 'https://raw.githubusercontent.com/egersack/PS2HouseDataExercise/master/train.csv'
@@ -10,48 +10,49 @@ data = pd.read_csv(url)
 # only using the numerical data
 valid_numbers = data.select_dtypes(include=[np.number])
 # get rid of NaN values
-valid_numbers = valid_numbers.dropna(axis=0)#is axis 0 or 1? come back
+valid_numbers = valid_numbers.dropna(axis=1)#is axis 0 or 1? come back
 
 #getting the first 1000 
-numeric = valid_numbers.head(1000)
+first_numbers = valid_numbers.head(1000)
 
 # finding correlated data
-corr = numeric.corr()['SalePrice'].sort_values(ascending = False)
+corr = first_numbers.corr()['SalePrice'].sort_values(ascending = False)
 
-# create X and Y of numeric data and Sale Price to create linReg with
-X = numeric [corr]
+# make X and Y values that I can use for linear regression
+X = first_numbers [corr]
 Y = X['SalePrice']
 X = X.drop(['SalePrice'], axis = 1)
 
-# create lin reg
-model = LinearRegression().fit(X, Y)
+# create linear regression, imported above
+lr = linear_model.LinearRegression()
+model = lr.fit(X, Y)
+#predicting test data, should create a plot of the real sale price against the predicted sale price
+predictions = model.predict(X)
+plt.scatter(predictions, Y, color = 'r')
 
-# uncomment to predict test data and graph real sale price vs. predicted sale price
-#predictions = model.predict(X)
-#plt.scatter(predictions, Y, color = 'r')
-
-# test prediction validity on rest of training data - select end
-M = valid_numbers.tail(100)
-train_test = M.drop(['SalePrice'], axis = 1)
+# test prediction on last part of the training data
+last_numbers = valid_numbers.tail(100)
+training_testing = last_numbers.drop(['SalePrice'], axis = 1)
 # use model on train_test
-train_predict = model.predict(train_test)
+training_prediction = model.predict(training_testing)
 
-# scatterplot confirms validity of model. Uncomment to view plot
-#plt.scatter(M['SalePrice'], train_predict, color = 'r')
+# scatterplot 
+plt.scatter(last_numbers['SalePrice'], training_prediction, color = 'r')
 
-# read in test data and select only numeric data
-test = pd.read_csv('test.csv')
-test_num =  test.select_dtypes(include=[np.number]).interpolate().dropna(axis=1)
+# url from test data and select only numeric data
+url = 'https://raw.githubusercontent.com/egersack/PS2HouseDataExercise/master/test.csv'
+test_data = pd.read_csv(url)
+test_numbers =  test_data.select_dtypes(include=[np.number])
+test_numbers = test_numbers.dropna(axis=1)
 
 
-# create list of predicted sale prices
-test_predict = model.predict(test_num)
-# Round prices to the nearest cent
-testPredictRounded = np.around(test_predict, decimals=2)
+# create list of predicted sale prices, then round the values to two decimal places
+list_predicted_prices = model.predict(test_numbers)
+list_predict_rounded = np.around(list_predicted_prices, decimals=2)
 # add predicted price to test table
-test['SalePrice'] = testPredictRounded
+test_data['SalePrice'] = list_predict_rounded
 
-finalOutput = test[['Id', 'SalePrice']]
+finalOutput = test_data[['Id', 'SalePrice']]
 #print predicted price and Id for user
 print(finalOutput.to_string(index=False))
 
