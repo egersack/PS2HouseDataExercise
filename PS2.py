e3e96d0c8e341956c404618fc556dbaff72fbeb7
egersack <egersack@mit.edu>
2022-02-25 10:17:12 -0500

    commit

--- PS2.py
+++ PS2.py
@@ -4,7 +4,6 @@ from sklearn import linear_model
 
 # raw data from train.csv
 url = 'https://raw.githubusercontent.com/egersack/PS2HouseDataExercise/master/train.csv'
-
 data = pd.read_csv(url)
 
 # only using the numerical data
@@ -19,16 +18,16 @@ first_numbers = valid_numbers.head(1000)
 corr = first_numbers.corr()['SalePrice'].sort_values(ascending = False)
 
 # make X and Y values that I can use for linear regression
-X = first_numbers [corr]
-Y = X['SalePrice']
-X = X.drop(['SalePrice'], axis = 1)
+Y = first_numbers ['SalePrice']
+X = first_numbers.drop(['SalePrice'], axis = 1)
 
 # create linear regression, imported above
 lr = linear_model.LinearRegression()
 model = lr.fit(X, Y)
-#predicting test data, should create a plot of the real sale price against the predicted sale price
-predictions = model.predict(X)
-plt.scatter(predictions, Y, color = 'r')
+
+##predicting test data, should create a plot of the real sale price against the predicted sale price
+#predictions = model.predict(X)
+#plt.scatter(predictions, Y, color = 'r')
 
 # test prediction on last part of the training data
 last_numbers = valid_numbers.tail(100)
@@ -36,8 +35,8 @@ training_testing = last_numbers.drop(['SalePrice'], axis = 1)
 # use model on train_test
 training_prediction = model.predict(training_testing)
 
-# scatterplot 
-plt.scatter(last_numbers['SalePrice'], training_prediction, color = 'r')
+## scatterplot 
+#plt.scatter(last_numbers['SalePrice'], training_prediction, color = 'r')
 
 # url from test data and select only numeric data
 url = 'https://raw.githubusercontent.com/egersack/PS2HouseDataExercise/master/test.csv'
@@ -45,10 +44,9 @@ test_data = pd.read_csv(url)
 test_numbers =  test_data.select_dtypes(include=[np.number])
 test_numbers = test_numbers.dropna(axis=1)
 
-
 # create list of predicted sale prices, then round the values to two decimal places
 list_predicted_prices = model.predict(test_numbers)
-list_predict_rounded = np.around(list_predicted_prices, decimals=2)
+list_predict_rounded = np.around(list_predicted_prices, decimals=2)#np.around "evenly round to the given number of decimals"
 # add predicted price to test table
 test_data['SalePrice'] = list_predict_rounded
 
